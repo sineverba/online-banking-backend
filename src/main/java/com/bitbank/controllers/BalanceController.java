788b64384b84f697487ea7ada32b940fa7fcfd7c
Alessandro Arici <alessandro.arici@gmail.com>
2022-02-26 09:02:08 +0100

    Add Swagger

--- src/main/java/com/bitbank/controllers/BalanceController.java
+++ src/main/java/com/bitbank/controllers/BalanceController.java
@@ -11,14 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bitbank.responses.BalanceResponse;
 import com.bitbank.services.BankAccountTransactionsService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/v1/balance")
+@Tag(name = "Balance", description = "Balance")
 public class BalanceController {
 
 	@Autowired
 	private BankAccountTransactionsService bankAccountTransactionsService;
 
 	@GetMapping
+	@Operation(security = { @SecurityRequirement(name = "bearer-key") })
 	public ResponseEntity<BalanceResponse> show() {
 		BigDecimal balance = bankAccountTransactionsService.balance();
 		return ResponseEntity.ok(new BalanceResponse(balance));
