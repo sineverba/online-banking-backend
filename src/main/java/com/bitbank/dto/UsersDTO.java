57fb276adb46d224998c5593e92d3c3bc465915d
Alessandro Arici <alessandro.arici@gmail.com>
2022-02-28 08:23:54 +0100

    Add login validation

--- src/main/java/com/bitbank/dto/UsersDTO.java
+++ src/main/java/com/bitbank/dto/UsersDTO.java
@@ -1,12 +1,19 @@
 package com.bitbank.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class UsersDTO {
-	
+
+	@NotNull(message = "has to be present")
+	@NotEmpty(message = "has to be present")
 	private String username;
-	
+
+	@NotNull(message = "has to be present")
+	@NotEmpty(message = "has to be present")
 	private String password;
-	
+
 }
