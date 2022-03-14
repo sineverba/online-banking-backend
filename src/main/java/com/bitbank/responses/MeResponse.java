7cee5685368a18aeb9eac3dfd3547d1c398249c5
Alessandro Arici <alessandro.arici@gmail.com>
2022-03-14 22:39:03 +0100

    Add me route

---
+++ src/main/java/com/bitbank/responses/MeResponse.java
@@ -0,0 +1,15 @@
+package com.bitbank.responses;
+
+public class MeResponse {
+
+	private String username;
+
+	public MeResponse(String username) {
+		this.username = username;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+}
